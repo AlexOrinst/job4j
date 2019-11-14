c90210d844943d77b1a9aff073322d9fee5f58b7
AlexOrinst <koluchiy8@gmail.com>
2019-11-14 20:14:12 +0300

    Отладка программы в IDEA[#196740]

--- chapter_001/src/main/java/ru/job4j/bot/DummyBot.java
+++ chapter_001/src/main/java/ru/job4j/bot/DummyBot.java
@@ -5,9 +5,9 @@ public class DummyBot {
     public static String answer (String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос";
         if ("Привет, бот".equals(question)) {
-            return "Привет, умник";
+            rsl = "Привет, умник";
         }else if ("Пока, бот".equals(question)) {
-            return "До скорой встречи";
+            rsl = "До скорой встречи";
         }
         return rsl;
 
