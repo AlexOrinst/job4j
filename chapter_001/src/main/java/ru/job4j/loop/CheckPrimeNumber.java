bf7f8ed3702a6b6657a2f34219657234fac02144
AlexOrinst <koluchiy8@gmail.com>
2019-11-25 19:38:27 +0300

    Простое число.[#196752]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,12 +3,12 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
 
     public boolean isPrime(int num) {
-        boolean isPrime = false;
+        boolean isPrime = true;
         for (int i = 2; i < num; i++) {
             if (num % i == 0) {
+                isPrime = false;
                 break;
             }
-            isPrime = true;
         }
         return isPrime;
     }
--- chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -1,4 +1,5 @@
 package ru.job4j.loop;
+
 import junit.framework.Assert;
 import org.junit.Test;
 
@@ -6,36 +7,42 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 
-
 public class CheckPrimeNumberTest {
 
     @Test
-    public void when5 () {
+    public void when5() {
         CheckPrimeNumber check = new CheckPrimeNumber();
         boolean res = check.isPrime(5);
         assertThat(res, is(true));
     }
+
     @Test
-    public void when3 () {
+    public void when3() {
         CheckPrimeNumber check = new CheckPrimeNumber();
         boolean res = check.isPrime(3);
         assertThat(res, is(true));
     }
 
     @Test
-    public void when4 () {
+    public void when2() {
+        CheckPrimeNumber check = new CheckPrimeNumber();
+        boolean res = check.isPrime(2);
+        assertThat(res, is(true));
+    }
+
+    @Test
+    public void when4() {
         CheckPrimeNumber check = new CheckPrimeNumber();
         boolean res = check.isPrime(4);
         assertThat(res, is(false));
     }
 
     @Test
-    public void when6 () {
+    public void when6() {
         CheckPrimeNumber check = new CheckPrimeNumber();
         boolean res = check.isPrime(6);
         assertThat(res, is(false));
     }
-
 
 
 }
